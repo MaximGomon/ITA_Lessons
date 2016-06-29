f74685f554d562aab6fc80966ac09323f40132a6
Maxim Gomon <maksym.gomon@gmail.com>
2016-06-29 12:38:24 +0300

    Add Ado.Net solution

---
+++ AdoNetSample/AdoNetSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private void ReadFromBase()
+        {
+            var conString = ConfigurationManager.AppSettings["ConnectionString"];
+
+            using (var connection = new SqlConnection(conString))
+            {
+                
+            }
+        }
+    }
+}
