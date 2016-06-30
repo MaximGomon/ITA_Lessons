f17241ef7cf577834807d678a06c111378b54d3c
Maxim Gomon <maksym.gomon@gmail.com>
2016-06-30 10:16:30 +0300

    Add reader from db to dynamic list

--- AdoNetSample/AdoNetSample/Program.cs
+++ AdoNetSample/AdoNetSample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,41 @@ namespace AdoNetSample
 
             using (var connection = new SqlConnection(conString))
             {
-                
+                string commandText = @"SELECT DISTINCT
+                                      e.Id, e.Name, e.Birthday, e.Email, e.Salary
+                                    FROM
+                                      Employees e
+                                    WHERE 
+                                      LEN(e.Name) > @len";
+                var command = new SqlCommand(commandText);
+                var parameter = new SqlParameter("@len", SqlDbType.Int);
+                parameter.Value = 20;
+
+                command.Parameters.Add(parameter);
+
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    List<dynamic> dataFromDb = new List<dynamic>();
+                    while (reader.Read())
+                    {
+                        var item = new
+                        {
+                            Id = int.Parse(reader[0].ToString()),
+                            Name = reader[1].ToString(),
+                            BirthDay = reader[2].GetType() != typeof (DBNull)
+                                ? Convert.ToDateTime(reader[2])
+                                : new DateTime(),
+                            Email = reader[3].GetType() != typeof (DBNull)
+                                ? reader[3].ToString()
+                                : String.Empty,
+                            Salary = reader[4].GetType() != typeof(DBNull)
+                            ? reader[4].ToString()
+                            : String.Empty
+                        };
+                        dataFromDb.Add(item);
+                    }
+                }
             }
         }
     }
