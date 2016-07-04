5b244c4a986b497b98200e0836140989852f4e5c
Maxim Gomon <maksym.gomon@gmail.com>
2016-07-04 19:33:34 +0300

    Add comments to code

--- AdoNetSample/AdoNetSample/Program.cs
+++ AdoNetSample/AdoNetSample/Program.cs
@@ -10,6 +10,7 @@ namespace AdoNetSample
     {
         static void Main(string[] args)
         {
+            //Call function for read data from db to List<Employee>
             var data = ReadFromBase();
             foreach (var employee in data)
             {
@@ -20,31 +21,43 @@ namespace AdoNetSample
 
         private static List<Employee> ReadFromBase()
         {
+            //read string from app.config file with key "ConnectionString"
             var conString = ConfigurationManager.AppSettings["ConnectionString"];
+            //create list for save data from db and return as result of function
             List<Employee> dataFromDb = new List<Employee>();
+
+            //create and use connection to SQL db
             using (var connection = new SqlConnection(conString))
             {
+                //create command text whith we will use in SQLCommand
                 string commandText = @"SELECT DISTINCT
                                       e.Id, e.Name, e.Birthday, e.Email, e.Salary
                                     FROM
                                       Employees e
                                     WHERE 
                                       LEN(e.Name) > @len";
+                //create SqlCommand with early entered text and connection
                 var command = new SqlCommand(commandText, connection);
+                //create parameter for our command
                 var parameter = new SqlParameter("@len", SqlDbType.Int);
+                //set parameter value
                 parameter.Value = 20;
-
+                //add created parameter to command
                 command.Parameters.Add(parameter);
-
+                //open sql connection
                 connection.Open();
+                //execute and using reader for get results of command executing
                 using (var reader = command.ExecuteReader())
                 {
+                    //read data from reader
                     while (reader.Read())
                     {
+                        //create new Employee from db data
                         var item = new Employee
                         {
                             Id = int.Parse(reader[0].ToString()),
                             Name = reader[1].ToString(),
+                            //check if data in column "e.Birthday" not null. If it is true - return new DateTime()
                             BirthDate = reader[2].GetType() != typeof (DBNull)
                                 ? Convert.ToDateTime(reader[2])
                                 : new DateTime(),
@@ -55,10 +68,12 @@ namespace AdoNetSample
                             ? reader[4].ToString()
                             : String.Empty
                         };
+                        //add created employee to result list
                         dataFromDb.Add(item);
                     }
                 }
             }
+            //return result list
             return dataFromDb;
         }
     }
