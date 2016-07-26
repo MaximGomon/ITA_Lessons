b13621fffb2dfc072a76309d283c438d5eadabfb
Maxim Gomon <maksym.gomon@gmail.com>
2016-07-26 11:31:14 +0300

    Add project WcfExample

---
+++ WcfExample/WcfExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfExample.CalcServiceReference;
+
+namespace WcfExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new CalcServiceClient();
+            Console.WriteLine(client.Sum(3, 4));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WcfExample/WcfServer/CalcService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CalcService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CalcService.svc or CalcService.svc.cs at the Solution Explorer and start debugging.
+    public class CalcService : ICalcService
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
