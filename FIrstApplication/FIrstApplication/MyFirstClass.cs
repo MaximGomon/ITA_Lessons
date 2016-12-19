dcaa4af6cec463a39839162ae1ee9ac08ead9981
Maxim Gomon <maksym.gomon@gmail.com>
2016-12-19 20:09:01 +0200

    Add project FirstAplication

---
+++ FIrstApplication/FIrstApplication/MyFirstClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyFIrstApplication
+{
+    public class MyFirstClass
+    {
+        public void DisplayText()
+        {
+            System.Console.WriteLine("I am text!");
+        }
+
+        public static void DisplayStaticText()
+        {
+            Console.WriteLine("Static text");
+        }
+
+        public int Number { get; set; }
+        public string Text { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Number} {Text}";
+        }
+    }
+}
