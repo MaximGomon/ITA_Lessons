cab47be253dc4728440f5bfc9cd3bd09770e2254
Maxim Gomon <maksym.gomon@gmail.com>
2017-02-28 17:29:51 +0200

    Add DllImport exmple

---
+++ DllImportExample/DllImportExample/ImportedFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace DllImportExample
+{
+    public class ImportedFunctions
+    {
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern ushort GetVersion();
+
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern DriveType GetDriveType([MarshalAs(UnmanagedType.LPStr)] string lpRootPathName);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetActiveWindow();
+    }
+}
---
+++ DllImportExample/DllImportExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllImportExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driveType = ImportedFunctions.GetDriveType("D:\\");
+            Console.WriteLine("Drive Type is: {0}", driveType);
+
+            var systemVersion = ImportedFunctions.GetVersion();
+            Console.WriteLine("System version is: {0}", systemVersion);
+            Console.ReadLine();
+        }
+    }
+}
