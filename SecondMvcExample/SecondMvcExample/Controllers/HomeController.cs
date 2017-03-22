f303a4990d2af82fe0c7be4d7a29358702d588e1
Maxim Gomon <maksym.gomon@gmail.com>
2017-03-22 10:33:24 +0200

    Add some new example

---
+++ SecondMvcExample/SecondMvcExample/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SecondMvcExample.Models;
+
+namespace SecondMvcExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Display()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Animal item)
+        {
+            return RedirectToAction("Display");
+        }
+    }
+}
---
+++ SecondMvcExample/SecondMvcExample/Models/Animal.cs
@@ -0,0 +1,7 @@
+﻿namespace SecondMvcExample.Models
+{
+    public class Animal
+    {
+        public string Name { get; set; } 
+    }
+}
